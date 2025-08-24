73c7a130bc424b35d4fbae474fb76bc6d3bb6bed
Pradeep9550 <kapil95550@gmail.com>
2025-08-24 22:35:32 +0530

    uploas file with multer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,13 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import dotenv from 'dotenv';
-
-
-dotenv.config();
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_URL, 
+        target: "http://localhost:300"
       },
     },
   },
